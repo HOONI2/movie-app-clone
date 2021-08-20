1b9e26cda0afb8ca6243e999c1dfe29423ed027d
unknown <singersman@naver.com>
2021-08-20 17:52:56 +0900

    GridCards

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -1,2 +1,6 @@
 //SERVER ROUTES
-export const USER_SERVER = '/api/users';
+export const USER_SERVER = "/api/users";
+
+export const API_URL = "https://api.themoviedb.org/3/";
+export const API_KEY = "983dbc86501093ad8f3145686511f69c";
+export const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/";
