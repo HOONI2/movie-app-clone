7d7f9877ef1b8f0d3018cd75c669f11765ccdea1
unknown <singersman@naver.com>
2021-08-30 16:07:00 +0900

    favorite

--- client/src/components/views/MovieDetail/Sections/Favorite.js
+++ client/src/components/views/MovieDetail/Sections/Favorite.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Axios from "axios";
 function Favorite(props) {
   const movieId = props.movieId;
@@ -7,6 +7,9 @@ function Favorite(props) {
   const moviePost = props.movieInfo.backdrop_path;
   const movieRunTime = props.movieInfo.runtime;
 
+  const [FavoriteNumber, setFavoriteNumber] = useState(0);
+  const [Favorited, setFavorited] = useState(false);
+
   useEffect(() => {
     let variable = {
       userFrom,
@@ -14,15 +17,28 @@ function Favorite(props) {
     };
     Axios.post("/api/favorite/favoriteNumber", variable).then((response) => {
       if (response.data.success) {
+        console.log("favoriteNumber", response.data);
+        setFavoriteNumber(response.data.favoriteNumber);
       } else {
         alert("숫자 정보를 가져오는데 실패했습니다");
+      }
+    });
+
+    Axios.post("/api/favorite/favorited", variable).then((response) => {
+      if (response.data.success) {
+        console.log("favorited", response.data);
+        setFavorited(response.data.favorited);
+      } else {
+        alert("정보를 가져오는데 실패했습니다");
       }
     });
   }, []);
 
   return (
     <div>
-      <button>Favorite</button>
+      <button>
+        {Favorited ? "Not Favorite" : "Add to Favorite"} {FavoriteNumber}
+      </button>
     </div>
   );
 }
